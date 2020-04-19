c0f14570f3d9ec8e3489a2aa349a02227071b646
GenWatt <adrian.nowy@op.pl>
2020-04-19 20:22:28 +0200

    Quiz

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./script.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist/js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+
+        loader: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "sass-loader"],
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin("./style.css"),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
